76abca399a736b9f57aa76a98f4b7783b3ff6d79
AndersonRFerreira <andersonn.reis90@gmail.com>
2021-09-22 22:12:20 -0300

    mudanças 02

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from '.Components/Header/Header';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Seu Carrinho | PneuStore</title>
+        <title>Seu Carrinho | PneuStore </title>
         <meta name="description" content="PneuStore -O pneu ideal para o seu Veículo e moto" />
         <link rel="icon" href="/favicon.ico" />  
       </Head>
+      <Header/>
 
 
-     
     </div>
   )
 }
