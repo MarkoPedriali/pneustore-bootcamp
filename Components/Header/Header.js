445f48ada32dbdeefa50d8dbca28d9aa00b3953c
MarkoPedriali <marko.pedriali@hotmail.com>
2021-09-26 20:55:06 -0300

    commit header refeito(mobile-first).

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,4 +1,4 @@
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 
 export default function Header(){
     return(
@@ -9,7 +9,7 @@ export default function Header(){
                     SAC 0800 602 2013
                 </li>
                 <li>
-                    ? Perguntas Frequentes
+                    Perguntas Frequentes
                 </li>
                 <li>
                     Contato
@@ -20,11 +20,20 @@ export default function Header(){
                 <div className={styles.logo}>
                     <img src="PneuStore.png" />
                 </div>
-                <div className={styles.input}>
-                    <input type= "text" placeholder='  Pesquise por marca e medida. exemplo(175/65)'></input>
+                <div className={styles.user}>
+                    <div className={styles.login}>
+                        <img src="login.png" className={styles.icon} alt="login"/>
+                    </div>
+                    <div className={styles.cart}>
+                        <img src="cart.png" className={styles.icon} alt="carrinho de compras"/>
+                        <p><strong>2</strong></p>
+                    </div>
                 </div>
-                <div className={styles.login}>
-                    <img src="login.png"/>
+                <div className={styles.search}>
+                    <input type= "text" placeholder='  Pesquise por marca e medida. exemplo(175/65)'></input>
+                    <div className={styles.searchIcon}>
+                        <img src="search.png" className={styles.icon} alt="botao de pesquisa"/>
+                    </div>
                 </div>
             </div>
         </div>
